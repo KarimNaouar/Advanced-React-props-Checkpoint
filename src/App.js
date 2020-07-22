bcd129e809725a605326cc5c07b5018c3b9cd897
KarimNaouar <KarimNaouar@users.noreply.github.com>
2020-07-22 12:42:04 +0200

    Advanced-React-props-Checkpoint

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import "./style.css";
+
+const products = [
+  { price: "125", name: "product1", category: "clothes" },
+  { price: "524", name: "product2", category: "electronic" },
+  { price: "625", name: "product3", category: "clothes" },
+  { price: "25", name: "product4", category: "clothes" },
+];
+const ProductTable = (props) => {
+  return (
+    <>
+      {props.products.map((el, i) => (
+        <tr key={i}>
+          <td>{el.name}</td>
+          <td>{el.price}</td>
+          <td>{el.category}</td>
+        </tr>
+      ))}
+    </>
+  );
+};
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <table>
+          <thead>
+            <tr>
+              <th>name</th>
+              <th>price</th>
+              <th>category</th>
+            </tr>
+          </thead>
+          <tbody>
+            <ProductTable products={products} />
+          </tbody>
+        </table>
       </header>
     </div>
   );
